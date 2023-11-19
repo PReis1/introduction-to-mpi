7692b13894f6bd566f19fa9bef0ba4e5ea6bb7bc
Pedro Henrique Reis <72507155+PReis1@users.noreply.github.com>
2023-11-19 15:01:33 -0300

    fix probing file

--- probing.cpp
+++ probing.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+#include <mpi.h>
+
+struct MPI_Struct {
+  int MPI_SOURCE;
+  int MPI_TAG;
+  int MPI_ERROR;
+  int _cancelled;
+  size_t _ucount;
+};
+
 void probing_process(int &int_sum, float &float_sum) {
   MPI_Status status;
   
@@ -13,12 +24,12 @@ void probing_process(int &int_sum, float &float_sum) {
 
   // 3- Add to int_sum or float_sum depending on the tag of the message
   if (tag == 0) {
-    int message;
-    MPI_Recv(&message, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-    *int_sum += message;
+    int message_int;
+    MPI_Recv(&message_int, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+    int_sum += message_int;
   } else if (tag == 1) {
-    float message;
-    MPI_Recv(&message, 1, MPI_FLOAT, source, tag, MPI_COMM_WORLD, &status);
-    *float_sum += message;
+    float message_float;
+    MPI_Recv(&message_float, 1, MPI_FLOAT, source, tag, MPI_COMM_WORLD, &status);
+    float_sum += message_float;
   }
 }
